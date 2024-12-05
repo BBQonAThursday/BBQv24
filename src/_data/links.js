37467e91d26a1cd50bd9fbdfdbb86eac3e554b68
Joel Gamble <j.gamble@conversionsciences.com>
2024-12-05 13:21:31 -0600

    updates

---
+++ src/_data/links.js
@@ -0,0 +1,35 @@
+const links = [
+  {
+    title: 'css box shadow examples',
+    link: 'https://getcssscan.com/css-box-shadow-examples',
+    tags: ['css', 'box-shadow']
+  },
+  {
+    title: 'SVG Backgrounds & Animations',
+    link: 'https://www.svgbackgrounds.com/css-box-shadow-examples',
+    tags: ['svg', 'animation', 'loader']
+  },
+  {
+    title: 'css Clamp Generator',
+    link: 'https://clamp-generator.netlify.app/',
+    tags: ['responsive', 'css', 'clamp']
+  },
+  {
+    title: 'Glide JS',
+    link: 'https://clamp-generator.netlify.app/',
+    info: ``
+    tags: ['js', 'library', 'slider']
+  },
+  {
+    title: 'CSS Position Sticky Solved',
+    link: 'https://dev.to/robmarshall/how-to-fix-issues-with-css-position-sticky-not-working-4a18',
+    info: ``
+    tags: ['sticky', 'css']
+  },{
+    title: 'GTM - Load Script on First Interaction',
+    link: 'https://www.marius.digital/gtm-load-scripts-on-scroll/',
+    info: `GTM guide on triggering a script to load on first user interaction.`
+    tags: ['js', 'gtm']
+  }
+  
+]
